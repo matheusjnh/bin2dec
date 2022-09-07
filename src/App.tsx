c696f9f197b424f49fcc36531ce0f85c5661372b
Matheus Santos <matheusjnh@gmail.com>
2022-09-06 21:18:18 -0300

    insert id for inputs

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ export function App() {
           }}
         >
           <InputText
+            id="bin"
             placeholder="Binary"
             style={{
               flex: 1,
@@ -22,6 +23,7 @@ export function App() {
           />
 
           <InputText
+            id="dec"
             placeholder="Decimal"
             style={{
               flex: 1,
