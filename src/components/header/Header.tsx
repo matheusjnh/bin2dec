98571987ca2b51ae2322d9bf36eaae1772c13e0e
Matheus Santos <matheusjnh@gmail.com>
2022-09-06 23:58:10 -0300

    create header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import "./HeaderStyles.css";
+
+export function Header() {
+  return (
+    <header className="l-header">
+      <h1 className="l-header__title">B2D - Binary to Decimal</h1>
+    </header>
+  );
+}
