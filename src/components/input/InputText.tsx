2b1865801052cdf70aa07931e4b912ae433a0e73
Matheus Santos <matheusjnh@gmail.com>
2022-09-06 21:15:23 -0300

    add id property to InputText component

--- src/components/input/InputText.tsx
+++ src/components/input/InputText.tsx
@@ -1,14 +1,16 @@
 import "./InputTextStyles.css";
 
 type InputProps = {
+  id: string;
   placeholder?: string;
   style?: { [key: string]: string | number };
 };
 
-export function InputText({ placeholder, style }: InputProps) {
+export function InputText({ id, placeholder, style }: InputProps) {
   return (
     <input
       className="input-text"
+      id={id}
       type="text"
       placeholder={placeholder}
       style={style}
